4da7641d5ba7da9ecd2f26ec0930edaf827e519e
Josef Zacek <info@josefzacek.com>
2016-10-29 01:48:25 +0100

    get JSON feed form url on store it in 'url' variable

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
+
+  // get url with json
+  // https://crossorigin.me/ is used to fix No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'null' is therefore not allowed access.
+  var url = 'https://crossorigin.me/http://adminstaging.bpcloud.searchoptics.net/blueprintcms/sites/1472c6e0482f9d9fe1bf7b70cc6f8c78/data/datalayer.json';
     
 });
