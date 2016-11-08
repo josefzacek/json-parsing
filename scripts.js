edb642c678f6facc7826b9e0c39f5d97d129854b
Josef Zacek <info@josefzacek.com>
2016-11-08 00:17:24 +0000

    pull JSON feed from local file called feed.json

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
 
-  // get url with json
-  // https://crossorigin.me/ is used to fix No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'null' is therefore not allowed access.
-  var url = 'https://crossorigin.me/http://adminstaging.bpcloud.searchoptics.net/blueprintcms/sites/1472c6e0482f9d9fe1bf7b70cc6f8c78/data/datalayer.json';
+  // get source with json
+  var source = 'feed.json';
 
   // Load JSON-encoded data from the server using a GET HTTP request.
   // http://api.jquery.com/jquery.getjson/
-  $.getJSON( url, function( data ) {
+  $.getJSON( source, function( data ) {
 
     // get top level
     category = data.Category;  // data['Category'];
