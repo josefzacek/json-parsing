301656324d9dd02e89be515228bc0c136d4c211e
Josef Zacek <info@josefzacek.com>
2016-12-01 20:37:28 +0000

    append product listing to .content-listing

--- scripts.js
+++ scripts.js
@@ -36,13 +36,13 @@ $(document).ready(function() {
       $('.content').append( '<div class="col-md-12 content-listing"></div>');
       $.each(product, function(key, value) {
         if (value.product_category === category || category === 'All' || category === 'Parse JSON' || category === 'Products Listing') {
-          $('.content').append('<h1><a href=' + value.product_id + ' class="product-detail" title="' + value.product_name + '"> ' + value.product_name + '</a></h1>');
-          $('.content').append('<p><b>Category:</b> ' + value.product_category + '</p>');
-          $('.content').append('<p><b>Short description:</b></p>');
-          $('.content').append('<p>'+ value.product_short_description + '</p>');
-          $('.content').append('<p><b>Price:</b> '+ value.product_price + '</p>');
-          $('.content').append('<p><small><a href=' + value.product_id + ' title="' + value.product_name + '" class="product-detail">  Read more... </a></small</p>');
-          $('.content').append('<hr>');
+          $('.content-listing').append('<h1><a href=' + value.product_id + ' class="product-detail" title="' + value.product_name + '"> ' + value.product_name + '</a></h1>');
+          $('.content-listing').append('<p><b>Category:</b> ' + value.product_category + '</p>');
+          $('.content-listing').append('<p><b>Short description:</b></p>');
+          $('.content-listing').append('<p>'+ value.product_short_description + '</p>');
+          $('.content-listing').append('<p><b>Price:</b> '+ value.product_price + '</p>');
+          $('.content-listing').append('<p><small><a href=' + value.product_id + ' title="' + value.product_name + '" class="product-detail">  Read more... </a></small</p>');
+          $('.content-listing').append('<hr>');
         }
       });
     }
